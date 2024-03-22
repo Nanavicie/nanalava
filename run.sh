20390858906790c370369978a35fc29bd2db5010
Nana <162071782+Nanavicie@users.noreply.github.com>
2024-03-22 17:10:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4165186d-fc53-4787-ab50-90614dddf115/)
 
 
 echo "$curl_output" >> Update.tsx
